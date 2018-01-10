2c9959414a24ec0de3d161832ff122fcf0037167
Kai Xia <xiaket@gmail.com>
2018-01-11 10:12:45 +1100

    add.
    Signed-off-by: Kai Xia <xiaket@gmail.com>

---
+++ python/anagram/anagram.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#encoding=utf8
+
+
+def detect_anagrams(word, candidates):
+    valid = lambda x: sorted(x) == sorted(word.lower()) and x != word.lower()
+    return [c for c in candidates if valid(c.lower())]
